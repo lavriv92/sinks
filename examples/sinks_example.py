d3e8bf85ed46e6ff63fdfdaa7fed0cd3327a8ee2
ivan lavriv <lavriv92@gmail.com>
2021-12-20 12:21:17 +0200

    Add various of operators

--- examples/sinks_example.py
+++ examples/sinks_example.py
@@ -9,7 +9,7 @@ import pprint
 import functools
 
 from sinks import Source
-from sinks.operators import extract_key, extract_partials, group_by, groupped
+from sinks.operators import extract_key, extract_partials, group_by, groupped, take_while, take
 
 
 def main():
@@ -33,6 +33,18 @@ def main():
           >> groupped(3)
           >> list
           >> pprint.pprint
+    )()
+
+    (
+        Source([1,2,3,4,5,6,7,8,9, 0])
+          >> take(4)
+          >> pprint.pprint
+    )()
+
+    (
+        Source([1,2,3,4,5,6,7,8])
+           >> take_while(lambda i: i <= 5)
+           >> pprint.pprint
     )()
 
 if __name__ == '__main__':
